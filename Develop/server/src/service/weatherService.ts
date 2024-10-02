5d6ec9c915d8ee99836d726cc3f9b191461c0340
Stephen Lee <leestephen0320@gmail.com>
2024-10-01 23:42:31 -0700

    did weatherService.ts todos until create destructureLocationData method

--- Develop/server/src/service/weatherService.ts
+++ Develop/server/src/service/weatherService.ts
@@ -4,21 +4,54 @@ dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
 interface Coordinates {
-
+  lon: string;
+  lat: string;
 }
 
 // TODO: Define a class for the Weather object
 interface Weather {
-  
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
 }
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL?: string;
+
+  private apiKey?: string;
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+
+    this.apiKey = process.env.API_KEY || '';
+  }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    try {
+      const response = await fetch(
+        `${this.baseURL}/data/2.5/weather?q=${query}&APPID=${this.apiKey}`
+      )
+
+      const locations = await response.json();
+      const mappedLocations = await this.destructureLocationData(locations.data);
+      return mappedLocations;
+    } catch (err) {
+      console.log('Error:',err);
+      return err;
+    }
+  }
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    const locationObject: Coordinates = {
+      lon: locationData.lon,
+      lat: locationData.lat,
+    };
+
+    return locationObject;
+  }
   // TODO: Create buildGeocodeQuery method
   // private buildGeocodeQuery(): string {}
   // TODO: Create buildWeatherQuery method
