e6f3bb058c008e925b3f9be70643381de9ee897f
Stephen Lee <leestephen0320@gmail.com>
2024-10-03 02:14:37 -0700

    finished weatherRoutes.ts todos i think and made progress on weatherService.ts have an idea i want to try

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,31 @@
 import { Router } from 'express';
 const router = Router();
-
-import HistoryService from '../../service/historyService.js';
-import WeatherService from '../../service/weatherService.js';
+//? is it necessary to make a historyroutes.js? I'm guessing we put both functions into this one file?
+//? will i need another historyService for the delete function?
+import HistoryService from '../../service/historyService.js'; 
+import weatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+//? is this how to get the city?
+router.post('/data/2.5/forecast?q=:city', async (req, res) => {
+  try {
   // TODO: GET weather data from city name
+  const cityName = req.params.city;
+  const weatherData = await weatherService.getWeatherForCity(cityName);
+  res.json(weatherData);
   // TODO: save city to search history
+  await HistoryService.addCity(cityName);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {
+router.get('/history', async (_req, res) => {
   try {
-    
+    const savedWeather = await HistoryService.getCities();
+    res.json(savedWeather);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -21,6 +33,17 @@ router.get('/history', async (req, res) => {
 });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({ msg: 'City id is required' });
+    }
+    await HistoryService.removeCity(req.params.id);
+    res.json({ success: 'City successfully removed from search history' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 export default router;
--- Develop/server/src/service/weatherService.ts
+++ Develop/server/src/service/weatherService.ts
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+// ? there are many more parameters, do i need all of them?
 interface Coordinates {
   lon: string;
   lat: string;
 }
 
 // TODO: Define a class for the Weather object
+//? there are like way more, do I need all of them?
+//! for now, just work with the parameters selected. see bottom of code for full list
 interface Weather {
   id: number;
   main: string;
@@ -36,8 +39,8 @@ class WeatherService {
       )
 
       const locations = await response.json();
-      const LocationData = await this.destructureLocationData(locations.data);
-      return LocationData;
+      const locationData = await this.destructureLocationData(locations.data);
+      return locationData;
     } catch (err) {
       console.log('Error:',err);
       return err;
@@ -56,8 +59,8 @@ class WeatherService {
   //? I feel like this part of the code should take some inputs, but it doesn't?
   //? added some properties, is this ok?
   private buildGeocodeQuery(coordinates: Coordinates): string {
-    let lat = coordinates.lat;
-    let lon = coordinates.lon;
+    let lat = this.destructureLocationData(coordinates).lat;
+    let lon = this.destructureLocationData(coordinates).lon;
     let query = `lat=${lat}&lon=${lon}`;
     return query;
   }
@@ -75,25 +78,101 @@ class WeatherService {
   // TODO: Create fetchWeatherData method
   private async fetchWeatherData(coordinates: Coordinates) {
     try {
-      
+      let query = this.buildWeatherQuery(coordinates);
       const response = await fetch(
         `${this.baseURL}/data/2.5/forecast?${query}&APPID=${this.apiKey}`
       )
 
-      const locations = await response.json();
-      const LocationData = await this.destructureLocationData(locations.data);
-      return LocationData;
+      const weatherRaw = await response.json();
+      const weatherData = await this.parseCurrentWeather(weatherRaw.data);
+      return weatherData;
     } catch (err) {
       console.log('Error:',err);
       return err;
     }
   }
   // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
+  //? 
+  private parseCurrentWeather(response: Weather[]) {
+    const weatherArray: Weather[] = response.map((weather) => {
+      const weatherObject: Weather = {
+        id: weather.id,
+        main: weather.main,
+        description: weather.description,
+        icon: weather.icon,
+      };
+
+      return weatherObject;
+    });
+
+    return weatherArray
+  }
   // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  //? i feel like i already ran the functionality of this code in the get by running parseCurrentWeather in fetchWeatherData
+  //private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    try {
+      const response = await fetch(
+        `${this.baseURL}/data/2.5/forecast?q=${city}&APPID=${this.apiKey}`
+      )
+
+      const weatherRaw = await response.json();
+      const weatherData = await this.parseCurrentWeather(weatherRaw.data);
+      return weatherData;
+    } catch (err) {
+      console.log('Error:',err);
+      return err;
+    }
+  }
 }
 
 export default new WeatherService();
+/*
+cod Internal parameter
+message Internal parameter
+cntA number of timestamps returned in the API response
+list
+list.dt Time of data forecasted, unix, UTC
+list.main
+list.main.temp Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+list.main.feels_like This temperature parameter accounts for the human perception of weather. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+list.main.temp_min Minimum temperature at the moment of calculation. This is minimal forecasted temperature (within large megalopolises and urban areas), use this parameter optionally. Please find more info here. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+list.main.temp_max Maximum temperature at the moment of calculation. This is maximal forecasted temperature (within large megalopolises and urban areas), use this parameter optionally. Please find more info here. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+list.main.pressure Atmospheric pressure on the sea level by default, hPa
+list.main.sea_level Atmospheric pressure on the sea level, hPa
+list.main.grnd_level Atmospheric pressure on the ground level, hPa
+list.main.humidity Humidity, %
+list.main.temp_kf Internal parameter
+list.weather
+list.weather.id Weather condition id
+list.weather.main Group of weather parameters (Rain, Snow, Clouds etc.)
+list.weather.description Weather condition within the group. Please find more here. You can get the output in your language. Learn more
+list.weather.icon Weather icon id
+list.clouds
+list.clouds.all Cloudiness, %
+list.wind
+list.wind.speed Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour
+list.wind.deg Wind direction, degrees (meteorological)
+list.wind.gust Wind gust. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour
+list.visibility Average visibility, metres. The maximum value of the visibility is 10km
+list.pop Probability of precipitation. The values of the parameter vary between 0 and 1, where 0 is equal to 0%, 1 is equal to 100%
+list.rain
+list.rain.3h Rain volume for last 3 hours, mm. Please note that only mm as units of measurement are available for this parameter
+list.snow
+list.snow.3h Snow volume for last 3 hours. Please note that only mm as units of measurement are available for this parameter
+list.sys
+list.sys.pod Part of the day (n - night, d - day)
+list.dt_txt Time of data forecasted, ISO, UTC
+city
+city.id City ID. Please note that built-in geocoder functionality has been deprecated. Learn more here
+city.name City name. Please note that built-in geocoder functionality has been deprecated. Learn more here
+city.coord
+city.coord.lat Geo location, latitude
+city.coord.lon Geo location, longitude
+city.country Country code (GB, JP etc.). Please note that built-in geocoder functionality has been deprecated. Learn more here
+city.population City population
+city.timezone Shift in seconds from UTC
+city.sunrise Sunrise time, Unix, UTC
+city.sunset Sunset time, Unix, UTC
+*/
